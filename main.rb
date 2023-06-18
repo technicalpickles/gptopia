9d8629c44dead02d1e5e910d6867e807461f2d11
Josh Nichols <joshua.nichols@gmail.com>
2023-06-18 11:47:14 -0400

    Start capturing conversation

--- main.rb
+++ main.rb
@@ -2,22 +2,58 @@ require "bundler/setup"
 Bundler.require(:default)
 Dotenv.load ".env"
 require "active_model"
+require "active_support/core_ext/string/inflections"
 
 llm = Langchain::LLM::OpenAI.new(api_key: ENV["OPENAI_API_KEY"])
 
-class Conversation
+class ChatModel
   include ActiveModel::API
+  include ActiveModel::Conversion
   include ActiveModel::Serialization
   include ActiveModel::Serializers::JSON
-
-  attr_accessor :name, :messages
-
+  include ActiveModel::Model
+  include ActiveModel::Validations
 
   def attributes=(hash)
     hash.each do |key, value|
       send("#{key}=", value)
     end
   end
+
+  def attributes
+    raise NotImplementedError
+  end
+end
+
+class Role
+  SYSTEM = :system
+  USER = :user
+  ASSISTANT = :assistant
+  FUNCTION = :function
+
+  VALID_SYMBOLS = [SYSTEM, USER, ASSISTANT, FUNCTION].freeze
+  VALID_STRINGS = VALID_SYMBOLS.map(&:to_s).freeze
+  VALID = (VALID_SYMBOLS + VALID_STRINGS).freeze
+end
+
+class Message < ChatModel
+  attr_accessor :role, :content
+
+  validates_inclusion_of :role, in: Role::VALID
+  validates :content, presence: true
+
+  def attributes
+    {
+      role: role,
+      content: content
+    }
+  end
+end
+
+class Conversation < ChatModel
+  attr_accessor :name, :messages
+
+  validates :name, presence: true
 
   def attributes
     {
@@ -28,13 +64,22 @@ class Conversation
 end
 
 
+prompt = TTY::Prompt.new
 
-messages = [
-  { role: "system", content: "You are a helpful assistant to help roleplaying game masters and players." },
-  { role: "user", content: "I want to run a campaign. It will be using Starfinder. How do I get started planning it?"}
-]
+name = prompt.ask "Name of conversation:"
+context = prompt.ask "Initial context:"
 
-conversation = Conversation.new(name: "Starting a Starfinder Game", messages: messages)
+messages = [Message.new(role: "system", content: context)]
+conversation = Conversation.new(name: name, messages: messages)
+
+loop do
+  user_message = prompt.ask(">")
+  if user_message == "done"
+    break
+  end
+  conversation.messages << Message.new(role: "user", content: user_message)
+end
+
 binding.pry
 
 # response = llm.chat messages: messages
